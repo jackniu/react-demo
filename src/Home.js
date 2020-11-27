75aedbec2984f4d68aeb84487b12dfcf4e70a30d
jackniu <chuangnzh@qq.com>
2020-11-27 14:51:45 +0800

    Add axios and render post list in Home page.

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,44 @@
 import React from 'react'
-import Rainbow from './hoc/Rainbow'
+// import Rainbow from './hoc/Rainbow'
+import axios from 'axios';
 
-const Home = () => {
-    return (
-        <div>
-            <div className="container">
-                <h1 className="center">Home页面</h1>
-                <p>欢迎来到龙马博客</p>
+class Home extends React.Component {
+    state = {
+        posts: [],
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+            console.log(res);
+            this.setState({
+                posts: res.data.slice(0, 20),
+            })
+        })
+    }
+    render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <div className="card-title">{post.title}</div>
+                            <div>{post.body}</div>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+                <div className="center">没有博客文章进行展示！</div>
+            );
+        return (
+            <div>
+                <div className="container">
+                    <h1 className="center">Home页面</h1>
+                    <div>{postList}</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default Rainbow(Home)
+export default Home
