5ddf0fed298b3d202f30a913981c40a774f4e1ea
jackniu <chuangnzh@qq.com>
2020-12-03 16:38:35 +0800

    Method 2: Use ThemeContext.Consumer to get state.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,18 +3,24 @@ import { ThemeContext } from '../contexts/ThemeContext'
 
 export class BookList extends Component {
     // 只有类组件才可以使用static，函数组件请参照官方文档；
-    static contextType = ThemeContext;
+    // static contextType = ThemeContext;
     render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
+
         return (
-            <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
-                <ul>
-                    <li style={{ background: theme.ui }}>HTML</li>
-                    <li style={{ background: theme.ui }}>CSS</li>
-                    <li style={{ background: theme.ui }}>JS</li>
-                </ul>
-            </div>
+            <ThemeContext.Consumer>{context => {
+                const { isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
+                return (
+                    <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+                        <ul>
+                            <li style={{ background: theme.ui }}>HTML</li>
+                            <li style={{ background: theme.ui }}>CSS</li>
+                            <li style={{ background: theme.ui }}>JS</li>
+                        </ul>
+                    </div>
+                )
+            }}
+            </ThemeContext.Consumer>
         )
     }
 }
