904782238384ead2feb285d7bc8fb50dd466264c
jackniu <chuangnzh@qq.com>
2020-12-04 10:45:24 +0800

    Add SongList component with function.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+const { default: SongList } = require("./components/SongList");
+
 function App() {
-  return (
-    <div className="App">
-      Demo
-    </div>
-  );
+    return (
+        <div className="App">
+            <SongList />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>少年 - 梦然</li>
+                <li>小苹果 - 筷子兄弟</li>
+                <li>琵琶行 - 沈谧仁 奇然</li>
+            </ul>
+        </div>
+    )
+}
+
+export default SongList
