0a8f30cc6c8a98e54071a01c2810d072d11a6515
jackniu <chuangnzh@qq.com>
2020-12-07 18:02:12 +0800

    Get mutiple context.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-class BookList extends Component {
-    // 只有类组件才可以使用static，函数组件请参照官方文档；
-    // static contextType = ThemeContext;
-    render() {
+// class BookList extends Component {
+//     // 只有类组件才可以使用static，函数组件请参照官方文档；
+//     // static contextType = ThemeContext;
+//     render() {
 
-        return (
-            <ThemeContext.Consumer>{context => {
-                const { isLightTheme, light, dark } = context;
-                const theme = isLightTheme ? light : dark;
-                return (
-                    <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
-                        <ul>
-                            <li style={{ background: theme.ui }}>HTML</li>
-                            <li style={{ background: theme.ui }}>CSS</li>
-                            <li style={{ background: theme.ui }}>JS</li>
-                        </ul>
-                    </div>
-                )
-            }}
-            </ThemeContext.Consumer>
-        )
-    }
+//         return (
+//             <ThemeContext.Consumer>{context => {
+//                 const { isLightTheme, light, dark } = context;
+//                 const theme = isLightTheme ? light : dark;
+//                 return (
+//                     <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+//                         <ul>
+//                             <li style={{ background: theme.ui }}>HTML</li>
+//                             <li style={{ background: theme.ui }}>CSS</li>
+//                             <li style={{ background: theme.ui }}>JS</li>
+//                         </ul>
+//                     </div>
+//                 )
+//             }}
+//             </ThemeContext.Consumer>
+//         )
+//     }
+// }
+
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+        <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+            <ul>
+                <li style={{ background: theme.ui }}>HTML</li>
+                <li style={{ background: theme.ui }}>CSS</li>
+                <li style={{ background: theme.ui }}>JS</li>
+            </ul>
+        </div>
+    )
 }
 
 export default BookList
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-class ThemeToggle extends Component {
-    render() {
-        return (
-            <ThemeContext.Consumer>{context => {
-                const { toggleTheme } = context;
-                return (
-                    <button onClick={toggleTheme}>切换主题样式</button>
-                )
-            }}
+// class ThemeToggle extends Component {
+//     render() {
+//         return (
+//             <ThemeContext.Consumer>{context => {
+//                 const { toggleTheme } = context;
+//                 return (
+//                     <button onClick={toggleTheme}>切换主题样式</button>
+//                 )
+//             }}
 
-            </ThemeContext.Consumer>
-        )
-    }
+//             </ThemeContext.Consumer>
+//         )
+//     }
+// }
+
+const ThemeToggle = () => {
+    const { toggleTheme } = useContext(ThemeContext);
+    return (
+        <button onClick={toggleTheme}>切换主题样式</button>
+    );
 }
 
 export default ThemeToggle
